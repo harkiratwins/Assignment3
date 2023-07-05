fa0526227418d5c3056256e4fff3b76282d5b71d
Harkirat <harkiratkaurwins@gmail.com>
2023-07-05 18:27:31 +0530

    chnge

--- src/Pages/NewTable.jsx
+++ src/Pages/NewTable.jsx
@@ -8,7 +8,6 @@ import "react-toastify/dist/ReactToastify.css";
 
 const baseURL = "http://localhost:4500/students";
 
-
 const showToastMessage = () => {
   toast.success("Successfully Deleted !", {
     position: toast.POSITION.TOP_RIGHT,
@@ -18,6 +17,7 @@ const showToastMessage = () => {
 const NewTable = () => {
   const [post, setPost] = useState([]);
   const [length, setlength] = useState(0);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     axios.get(baseURL).then((response) => {
@@ -25,7 +25,7 @@ const NewTable = () => {
     });
   }, [post]);
 
-  function deletePost(evt,id) {
+  function deletePost(evt, id) {
     // evt.stopPropagation()
     const conf = window.confirm("Do You Want To Delete");
     if (conf) {
@@ -37,96 +37,112 @@ const NewTable = () => {
           showToastMessage();
         })
         .catch((err) => console.log(err));
-        
     }
   }
 
   return (
     <>
-    <div className="App">
-      <div className="container">
-        <div className="table-wrapper">
-          <div className="table-title">
-            <div className="row">
-              <div className="col-sm-8">
-                <h2 style={{ textAlign: "left" }}>
-                Employee  <b>List</b>
-                </h2>
+      <div className="App">
+        <div className="container">
+          <div className="table-wrapper">
+            <div className="table-title">
+              <div className="row">
+                <div className="col-sm-6">
+                  <h2 style={{ textAlign: "left" }}>Employee List</h2>
+                </div>
+                <div className="col-sm-4" style={{ textAlign: "right" }}>
+                  <div class="search-box">
+                    <i class="material-icons">&#xE8B6;</i>
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Search&hellip;"
+                      onChange={(e) => setQuery(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="col-sm-2" style={{ textAlign: "right" }}>
+                  <CreateModel
+                    setPost={setPost}
+                    post={post}
+                    setlength={setlength}
+                  />
+                </div>
               </div>
 
-              <div className="col-sm-4" style={{ textAlign: "right" }}>
-                <CreateModel
-                  setPost={setPost}
-                  post={post}
-                  setlength={setlength}
-                />
-                {/* <Button
-                  type="button"
-                  //onClick={handleShow}
-                  className="btn btn-info add-new"
-                >
-                  <i className="fa fa-plus"></i> Add New
-                </Button> */}
-              </div>
+              {/* <div className="row">
+                <div className="col-sm-8"></div>
+                <div class="col-sm-4">
+                  <div class="search-box">
+                    <i class="material-icons">&#xE8B6;</i>
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Search&hellip;"
+                    />
+                  </div>
+                </div>
+              </div> */}
             </div>
-          </div>
 
-          <table className="table table-bordered">
-            <thead>
-              <tr>
-                <th>SR.No.</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Age</th>
-                <th>Gender</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {post.map((post, i) => (
-                <tr key={i}>
-                  <td>{post.id}</td>
-                  <td>{post.FirstName}</td>
-                  <td>{post.LastName}</td>
-                  <td>{post.Age}</td>
-                  <td>{post.Gender}</td>
-                  <td>
-                    <a
-                      href="D:\Harkirat kaur\Assignment3\project-3\src\Components\ViewModal\index.jsx"
-                      className="view"
-                      title="View"
-                    >
-                      <ViewModal id={post.id} post={post} />
-                    </a>
-                    <a
-                      href="D:\Harkirat kaur\Assignment3\project-3\src\Components\Edit"
-                      className="edit"
-                      title="Edit"
-                    >
-                      <EditModel
-                        id={post.id}
-                        post={post}
-                        setlength={setlength}
-                      />
-                    </a>
-                    <a href="aa" className="delete" title="Delete">
-                      <i
-                        onClick={(evnt) => deletePost(evnt,post.id)}
-                        className="material-icons"
-                      >
-                        &#xE872;
-                      </i>
-                    </a>
-                  </td>
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>SR.No.</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Age</th>
+                  <th>Gender</th>
+                  <th>Actions</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-          
+              </thead>
+              <tbody>
+                {post
+                  .filter((post) =>
+                    post.FirstName.toLowerCase().includes(query)
+                  )
+                  .map((post, i) => (
+                    <tr key={i}>
+                      <td>{post.id}</td>
+                      <td>{post.FirstName}</td>
+                      <td>{post.LastName}</td>
+                      <td>{post.Age}</td>
+                      <td>{post.Gender}</td>
+                      <td>
+                        <a
+                          href="D:\Harkirat kaur\Assignment3\project-3\src\Components\ViewModal\index.jsx"
+                          className="view"
+                          title="View"
+                        >
+                          <ViewModal id={post.id} post={post} />
+                        </a>
+                        <a
+                          href="D:\Harkirat kaur\Assignment3\project-3\src\Components\Edit"
+                          className="edit"
+                          title="Edit"
+                        >
+                          <EditModel
+                            id={post.id}
+                            post={post}
+                            setlength={setlength}
+                          />
+                        </a>
+                        <a href="aa" className="delete" title="Delete">
+                          <i
+                            onClick={(evnt) => deletePost(evnt, post.id)}
+                            className="material-icons"
+                          >
+                            &#xE872;
+                          </i>
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
-  
     </>
   );
 };
